6edd879026256c610ff32ec3c02051a5051b03bb
Erick Rick <erick.oliveira@ausland.com.br>
2024-06-24 14:20:48 -0300

    Inicinado proneto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Cotacao.Services;
+
+var cotacaoService = new ApiService(new HttpClient());
+DateTime dataHoje = DateTime.Now.AddDays(-3);
+// Console.WriteLine(cotacaoService.RetornarCotacaoDolarDia(dataHoje));
+
+// var caotacaoDia = await cotacaoService.RetornarCotacaoDolarDia(dataHoje);
+// Console.WriteLine(caotacaoDia.Erro);
+// Console.WriteLine(caotacaoDia.StatusCode);
+// Console.WriteLine(caotacaoDia.MensagemErro);
+// foreach (var item in caotacaoDia.Data!)
+//   Console.WriteLine(item.CotacaoVenda);
+
+var moedas = await cotacaoService.RetornarMoedas();
+var cotacaoMoedaDia = await cotacaoService.RetornarCotacaoMoedaDia("USD", dataHoje);
+
+
